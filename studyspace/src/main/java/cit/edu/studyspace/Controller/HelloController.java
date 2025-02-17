2a5a3c3f6902dd772a01afd08d90424970cf88d8
LAB14_USER ON WS42 <L14X18W42@citulab.net>
2025-02-17 20:49:32 +0800

    remove secret key

---
+++ studyspace/src/main/java/cit/edu/studyspace/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package cit.edu.studyspace.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the application!";
+    }
+ 
+    @GetMapping("/secured")
+    public String secured() {
+        return "This is a secured endpoint!";
+    }
+}
