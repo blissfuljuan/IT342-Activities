a8f5154180aa793f183c835fb0679747837e5f79
LAB14_USER ON WS27 <L14Y18W27@citulab.net>
2025-02-18 20:51:50 +0800

    Updated

--- G4-PaqueraRonanJake/oauth2login/oauth2login/src/main/java/com/paquera/oauth2login/Controller/UserController.java
+++ G4-PaqueraRonanJake/oauth2login/oauth2login/src/main/java/com/paquera/oauth2login/Controller/UserController.java
@@ -1,2 +1,18 @@
-package com.paquera.oauth2login.Controller;public class UserController {
+package com.paquera.oauth2login.Controller;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user-info")
+    public Map<String, Object> getUser(@AuthenticationPrincipal OAuth2User principal) {
+        return principal.getAttributes();
+    }
 }
