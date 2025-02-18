c6782083668db3eda1479e62cbacfaf8bd2f0c5a
LAB14_USER ON WS31 <L14Y18W31@citulab.net>
2025-02-18 20:43:48 +0800

    Github and Google Login 0auth2 (Final)

--- G4-Dakay-MiguelAntonio/oauth2login/src/main/java/com/miguelantonio/oauth2login/controller/UserController.java
+++ G4-Dakay-MiguelAntonio/oauth2login/src/main/java/com/miguelantonio/oauth2login/controller/UserController.java
@@ -1,2 +1,26 @@
-package com.miguelantonio.oauth2login.controller;public class UserController {
+package com.miguelantonio.oauth2login.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    @GetMapping
+    public String index() {
+        return "Welcome to testing landing page!";
+    }
+    @GetMapping("/user-info")
+    public Map<String, Object> getUser(@AuthenticationPrincipal OAuth2User principal) {
+        return principal.getAttributes();
+    }
+
+    @GetMapping("/secured")
+    public String wowsecured(@AuthenticationPrincipal OAuth2User principal) {
+        return "naol secured";
+    }
 }
