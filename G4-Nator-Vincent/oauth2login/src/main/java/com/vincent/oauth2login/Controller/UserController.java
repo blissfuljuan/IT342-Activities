3a27d2e1030549146406a456de424dbc8a891b25
LAB14_USER ON WS39 <L14Y18W39@citulab.net>
2025-02-18 20:44:37 +0800

    Spring Boot oAuth2 Login

--- G4-Nator-Vincent/oauth2login/src/main/java/com/vincent/oauth2login/Controller/UserController.java
+++ G4-Nator-Vincent/oauth2login/src/main/java/com/vincent/oauth2login/Controller/UserController.java
@@ -1,2 +1,28 @@
-package com.vincent.oauth2login.Controller;public class UserController {
+package com.vincent.oauth2login.Controller;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+
+    @GetMapping
+    public String index(){
+        return "Welcome Home";
+    }
+
+
+    @GetMapping("/user-info")
+    public Map<String, Object> getUser(@AuthenticationPrincipal OAuth2User principal){
+
+        return principal.getAttributes();
+    }
+
+
 }
--- G4-Nator-Vincent/oauth2login/src/main/java/com/vincent/oauth2login/Oauth2loginApplication.java
+++ G4-Nator-Vincent/oauth2login/src/main/java/com/vincent/oauth2login/Oauth2loginApplication.java
@@ -2,8 +2,11 @@ package com.vincent.oauth2login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+
 public class Oauth2loginApplication {
 
 	public static void main(String[] args) {
