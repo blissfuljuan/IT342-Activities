a52ab2f389145eb866f6374090d3303796e41e02
L14Y18W23 <gilesanthony.villamorii@cit.edu>
2025-02-18 20:39:35 +0800

    Spring Boot 0Auth Login Activity

---
+++ g4-villamor-giles/oauth2login/src/main/java/com/giles/oauth2login/controller/HelloController.java
@@ -0,0 +1,8 @@
+package com.giles.oauth2login.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+}
