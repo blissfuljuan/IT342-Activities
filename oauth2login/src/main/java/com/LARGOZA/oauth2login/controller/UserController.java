c462be891e9cc008ae43c218a00987293e0b1c15
LAB13_USER ON WS38 <L13X13W38@citulab.net>
2025-02-19 14:31:30 +0800

    Google OAuth

--- oauth2login/src/main/java/com/LARGOZA/oauth2login/controller/UserController.java
+++ oauth2login/src/main/java/com/LARGOZA/oauth2login/controller/UserController.java
@@ -1,2 +1,25 @@
-package com.LARGOZA.oauth2login.controller;public class UserController {
+package com.LARGOZA.oauth2login.controller;
+
+import java.util.Map;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping
+    public String index(){
+        return "<h1>Welcome, To the landing page!</h1>";
+    }
+
+    @GetMapping("/user-info")
+    public Map<String, Object> getUserProfile(@AuthenticationPrincipal OAuth2User oAuth2User){
+        return oAuth2User.getAttributes();
+    }
+
+    @GetMapping("/secured")
+    public String secureEndPoint() {
+        return "<h1>This is a secure endpoint.</h1>";
+    }
 }
