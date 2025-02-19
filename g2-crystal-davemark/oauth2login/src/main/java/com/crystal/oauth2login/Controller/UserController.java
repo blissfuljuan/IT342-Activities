4ecfd92fad2e2067295ba6340a4ec1fb35efa3ee
LAB13_USER ON WS09 <L13X13W09@citulab.net>
2025-02-19 14:31:23 +0800

    Working SpringBoot OAuth for Github and Google.

---
+++ g2-crystal-davemark/oauth2login/src/main/java/com/crystal/oauth2login/Controller/UserController.java
@@ -0,0 +1,28 @@
+package com.crystal.oauth2login.Controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    @GetMapping
+    public String index(){
+        return "<h1>Welcome, This is the landing page.</h1>";
+    }
+
+    @GetMapping("/user-info")
+    public Map<String, Object> getUserProfile(@AuthenticationPrincipal OAuth2User oAuth2User){
+        return oAuth2User.getAttributes();
+    }
+
+    @GetMapping("/secured")
+    public String securedEndpoint(){
+        return "<h1>This is a SECURED enpoint.</h1>";
+    }
+
+}
