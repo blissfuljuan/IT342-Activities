26d4b57fa7be145569b0d7650a72069dabc65af3
LAB13_USER ON WS39 <L13X13W39@citulab.net>
2025-02-19 14:32:09 +0800

    Google OAuth2

---
+++ G2-LOOD-JakeLuis/oauth2login/src/main/java/com/LOOD/oauth2login/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.LOOD.oauth2login.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(oauth -> oauth.anyRequest().authenticated())
+                .oauth2Login(oauth2Login -> oauth2Login.defaultSuccessUrl("/user-info", true))
+                .formLogin(formLogin -> formLogin.defaultSuccessUrl("/secured", true))
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
