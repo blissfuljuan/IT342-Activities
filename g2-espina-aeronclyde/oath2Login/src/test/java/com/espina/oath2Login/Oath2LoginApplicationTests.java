db6d78725437a440640026cfe0df935f0f43465d
LAB13_USER ON WS02 <L13X13W02@citulab.net>
2025-02-19 12:34:46 +0800

    Initial Springboot Oath to Login

---
+++ g2-espina-aeronclyde/oath2Login/src/test/java/com/espina/oath2Login/Oath2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espina.oath2Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oath2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
