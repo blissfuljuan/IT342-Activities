b2263969188551231d2f6826e05c4858fe0b489b
LAB14_USER ON WS19 <L14X08W19@citulab.net>
2025-02-19 08:29:13 +0800

    initial springboot Oauthlogin

---
+++ g3-Duterte-TerenceJohn/Duterte_OAuth2/OAuth2demo/src/main/java/duterte/com/example/OAuth2demo/OAuth2LoginApplication.java
@@ -0,0 +1,13 @@
+package duterte.com.example.OAuth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2demoApplication.class, args);
+	}
+
+}
