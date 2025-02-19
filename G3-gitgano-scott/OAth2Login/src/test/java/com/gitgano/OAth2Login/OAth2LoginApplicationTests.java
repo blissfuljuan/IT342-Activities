7516d53b66fc2e642868f3904b2a3e95391fa3b4
LAB14_USER ON WS41 <L14X08W41@citulab.net>
2025-02-19 08:33:37 +0800

    initial springboot oauth2login

---
+++ G3-gitgano-scott/OAth2Login/src/test/java/com/gitgano/OAth2Login/OAth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitgano.OAth2Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
