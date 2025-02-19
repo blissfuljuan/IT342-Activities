81eea2aec9e233ae4306b9e6acd1942c4adcfe8a
LAB14_USER ON WS01 <L14X08W01@citulab.net>
2025-02-19 08:22:41 +0800

    added maven

---
+++ G3-semilla-melenarafaella/oath2login/src/main/java/com/semilla/oath2login/OAth2LoginApplication.java
@@ -0,0 +1,13 @@
+package com.semilla.oath2login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAth2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAth2LoginApplication.class, args);
+	}
+
+}
