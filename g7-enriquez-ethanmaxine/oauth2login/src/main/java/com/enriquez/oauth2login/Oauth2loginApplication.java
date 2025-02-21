d28c7f069682bf4697f4ef91d07bf526a0963204
LAB14_USER ON WS04 <L14X20W04@citulab.net>
2025-02-21 19:00:20 +0800

    Initial Commit of Spring Boot Integration with OAuth2 for G7

--- g7-enriquez-ethanmaxine/oauth2login/src/main/java/com/enriquez/oauth2login/Oauth2loginApplication.java
+++ g7-enriquez-ethanmaxine/oauth2login/src/main/java/com/enriquez/oauth2login/Oauth2loginApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OAuth2LoginApplication {
+public class Oauth2loginApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OAuth2LoginApplication.class, args);
+		SpringApplication.run(Oauth2loginApplication.class, args);
 	}
 
 }
--- g7-enriquez-ethanmaxine/oauth2login/src/test/java/com/enriquez/oauth2login/Oauth2loginApplicationTests.java
+++ g7-enriquez-ethanmaxine/oauth2login/src/test/java/com/enriquez/oauth2login/Oauth2loginApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OAuth2LoginApplicationTests {
+class Oauth2loginApplicationTests {
 
 	@Test
 	void contextLoads() {
