ef5c323f6c39cb4f97b5a2050251c868cb26bbe0
LAB23_USER ON WS33 <L23X14W33@citulab.net>
2025-02-21 12:47:09 +0800

    Initial Spring boot commit

---
+++ g1-pusing-joshua/oath2login/src/test/java/com/pusing/oath2login/Oath2loginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pusing.oath2login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oath2loginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
