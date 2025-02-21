68563682ab3f80bbc52c0ac8459b6fa27cf94cd3
LAB14_USER ON WS11 <L14X20W11@citulab.net>
2025-02-21 20:53:23 +0800

    OAuth 2 login integration

--- g7-lacaba-seanjames/oauth2login/src/main/java/com/Lacaba/oauth2login/OAuth2loginApplication.java
+++ g7-lacaba-seanjames/oauth2login/src/main/java/com/Lacaba/oauth2login/OAuth2loginApplication.java
@@ -2,12 +2,20 @@ package com.Lacaba.oauth2login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 public class OAuth2loginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OAuth2loginApplication.class, args);
+
 	}
+
 
 }
