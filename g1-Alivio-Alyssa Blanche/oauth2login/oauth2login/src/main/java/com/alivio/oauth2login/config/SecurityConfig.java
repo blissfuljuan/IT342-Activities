ee146a3f086063d2f256521c5c412249f478bdb0
BlancheAlivio <alyssa.alivio@cit.edu>
2025-02-21 14:45:23 +0800

    Implement Spring Boot OAuth2 Integration

--- g1-Alivio-Alyssa Blanche/oauth2login/oauth2login/src/main/java/com/alivio/oauth2login/config/SecurityConfig.java
+++ g1-Alivio-Alyssa Blanche/oauth2login/oauth2login/src/main/java/com/alivio/oauth2login/config/SecurityConfig.java
@@ -1,2 +1,22 @@
-package com.alivio.oauth2login.config;public class SecurityConfig {
+package com.alivio.oauth2login.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/user-info", true))
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .build();
+    }
 }
--- g1-Alivio-Alyssa Blanche/oauth2login/oauth2login/src/main/java/com/alivio/oauth2login/controller/UserController.java
+++ g1-Alivio-Alyssa Blanche/oauth2login/oauth2login/src/main/java/com/alivio/oauth2login/controller/UserController.java
@@ -1,2 +1,23 @@
-package com.alivio.oauth2login.controller;public class UserController {
+package com.alivio.oauth2login.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String index(){
+        return "<h1> Welcome, this is the landing page.</h1>";
+    }
+
+    @GetMapping("/user-info")
+    public Map<String, Object> getUser(@AuthenticationPrincipal OAuth2User oAuth2User) {
+        return oAuth2User.getAttributes();
+    }
 }
