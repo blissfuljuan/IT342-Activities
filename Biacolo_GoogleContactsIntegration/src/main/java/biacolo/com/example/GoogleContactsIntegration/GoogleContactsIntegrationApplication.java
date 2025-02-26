aaf81e59043c6f71e3d86f85af03d6ee5517f877
LAB14_USER ON WS20 <L14X08W20@citulab.net>
2025-02-26 08:35:15 +0800

    Add Google Contacts Integration Activity

---
+++ Biacolo_GoogleContactsIntegration/src/main/java/biacolo/com/example/GoogleContactsIntegration/GoogleContactsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package biacolo.com.example.GoogleContactsIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleContactsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleContactsIntegrationApplication.class, args);
+	}
+
+}
