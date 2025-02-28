7107b98ff85de2fb6ce28f7f47a7aab9621634c6
LAB14_USER ON WS25 <L14X20W25@citulab.net>
2025-02-28 19:36:43 +0800

    Google Contacts

---
+++ g7-Yap-Francis/googlecontact/googlecontact/src/main/java/com/yap/googlecontact/GooglecontactApplication.java
@@ -0,0 +1,12 @@
+package com.yap.googlecontact;
+
+spring.security.user.name=user
+		spring.security.user.password=Xxxchromerazor21#
+
+		spring.security.oauth2.client.registration.github.client-id=Ov23lihhgJ9iVtPjmZim
+		spring.security.oauth2.client.registration.github.client-secret=d253871f7713f751cd8eb332d015dabc786da981
+		spring.security.oauth2.client.registration.github.scope=read:user,user.email
+
+		spring.security.oauth2.client.registration.google.client-id=
+		spring.security.oauth2.client.registration.google.client-secret=
+		spring.security.oauth2.client.registration.google.scope=profile,email
