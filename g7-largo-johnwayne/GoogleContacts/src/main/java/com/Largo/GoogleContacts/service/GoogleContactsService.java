4f0f68dd85083624635d99803c7a68017061e8b5
LAB14_USER ON WS10 <L14X20W10@citulab.net>
2025-02-28 20:44:53 +0800

    minor fixes

---
+++ g7-largo-johnwayne/GoogleContacts/src/main/java/com/Largo/GoogleContacts/service/GoogleContactsService.java
@@ -0,0 +1,16 @@
+package com.Largo.GoogleContacts.service;
+
+import com.Largo.GoogleContacts.model.Contacts;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GoogleContactsService {
+
+}
