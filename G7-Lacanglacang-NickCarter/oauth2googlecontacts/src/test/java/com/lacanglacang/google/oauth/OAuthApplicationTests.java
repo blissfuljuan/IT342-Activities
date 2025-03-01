1b142d6878f2c89f5d666815f192abcb302f533d
Kato-Neko <ng112403@gmail.com>
2025-03-02 01:31:33 +0800

    Read now available, both all and filtered. Made a modal for displaying information, and edit and delete buttons to be improved later.

---
+++ G7-Lacanglacang-NickCarter/oauth2googlecontacts/src/test/java/com/lacanglacang/google/oauth/OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacanglacang.google.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
