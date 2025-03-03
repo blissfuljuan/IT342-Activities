22d4ca2cfaee8aad4b19bf8fd6147237592e85d3
EmmanuelCagampang <emmanuel.cagampangjr@cit.edu>
2025-03-03 10:22:19 +0800

    Add files via upload

---
+++ CIT-UCommunityForums/src/main/java/edu/cit/CIT_UCommunityForums/model/Contact.java
@@ -0,0 +1,27 @@
+package edu.cit.CIT_UCommunityForums.model;
+
+public class Contact {
+    private String name;
+    private String email;
+
+    public Contact() { }
+
+    public Contact(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
