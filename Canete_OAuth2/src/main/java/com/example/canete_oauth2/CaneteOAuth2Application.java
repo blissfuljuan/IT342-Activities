60b4ba678a3a9f6cb24d3eb6cac203fd52e599fa
Xyrill Dereck Cañete <xdcanet@gmail.com>
2025-03-03 21:28:51 +0800

    Add project files

--- Canete_OAuth2/src/main/java/com/example/canete_oauth2/CaneteOAuth2Application.java
+++ Canete_OAuth2/src/main/java/com/example/canete_oauth2/CaneteOAuth2Application.java
@@ -1,4 +1,4 @@
-package com.example.canete_oauth2;
+package main.java.com.example.canete_oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Canete_OAuth2/src/test/java/com/example/canete_oauth2/CaneteOAuth2ApplicationTests.java
+++ Canete_OAuth2/src/test/java/com/example/canete_oauth2/CaneteOAuth2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.canete_oauth2;
+package test.java.com.example.canete_oauth2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
