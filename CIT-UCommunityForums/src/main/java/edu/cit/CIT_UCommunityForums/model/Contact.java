c707b9d8bee96233ebf559c029717480051228d8
EmmanuelCagampang <emmanuel.cagampangjr@cit.edu>
2025-03-03 10:37:12 +0800

    Add files via upload
    Partial Output 3/3/2025

--- CIT-UCommunityForums/src/main/java/edu/cit/CIT_UCommunityForums/model/Contact.java
+++ CIT-UCommunityForums/src/main/java/edu/cit/CIT_UCommunityForums/model/Contact.java
@@ -3,25 +3,44 @@ package edu.cit.CIT_UCommunityForums.model;
 public class Contact {
     private String name;
     private String email;
+    private String phone;
 
-    public Contact() { }
+    // No-arg constructor
+    public Contact() {
+    }
 
+    // New two-arg constructor
     public Contact(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
-    // Getters and setters
+    // Existing three-arg constructor
+    public Contact(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Getters & setters
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
