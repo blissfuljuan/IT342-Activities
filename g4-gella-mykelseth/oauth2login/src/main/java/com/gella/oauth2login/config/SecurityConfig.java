60b1a7d255d8d487fa2303631ad47a746755ef2a
MykelSeth <mykelseth.gella@cit.edu>
2025-03-05 00:56:41 +0800

    Added Google Contact API w CRUD
    Practical Exam for Midterm

--- g4-gella-mykelseth/oauth2login/src/main/java/com/gella/oauth2login/config/SecurityConfig.java
+++ g4-gella-mykelseth/oauth2login/src/main/java/com/gella/oauth2login/config/SecurityConfig.java
@@ -3,22 +3,24 @@ package com.gella.oauth2login.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig
-{
+public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
-    {
-        return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:8080/user-info", true))
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/user-info", true)
+                )
                 .logout(logout -> logout.logoutSuccessUrl("/"))
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
+                .formLogin(form -> form.defaultSuccessUrl("/user-info", true));
+
+        return http.build();
     }
 }
