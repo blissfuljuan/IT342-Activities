3c52d5d536ecf846ff5d5cf00572e12ad518c65c
Ehrica Jynne Espada <ehricajynne16@gmail.com>
2025-03-05 20:42:25 +0800

    Google Contacts
    Working CRUD for Google Contacts with API

--- g4-espada-ehricajynne/oauth2login/src/main/java/com/espada/oauth2login/config/SecurityConfig.java
+++ g4-espada-ehricajynne/oauth2login/src/main/java/com/espada/oauth2login/config/SecurityConfig.java
@@ -3,22 +3,33 @@ package com.espada.oauth2login.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .authorizeHttpRequests(auth ->
-                        auth.anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:8080/user-info", true))
-                .logout(logout -> logout.logoutSuccessUrl("/"))
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
-    }
-}
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/user-info", true)
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .formLogin(form -> form.defaultSuccessUrl("/user-info", true));
+
+        return http.build();
+    }
+
+
+
+}
