c39f1979f6c9c619e70ce7812dbdcd129ad330af
K1kks <mikaellorenzocuyugan@gmail.com>
2025-03-05 10:05:46 +0800

    Done with the exam activity

--- g2-cuyugan-mikael/oauth2login/src/main/java/com/cuyugan/oauth2login/config/SecurityConfig.java
+++ g2-cuyugan-mikael/oauth2login/src/main/java/com/cuyugan/oauth2login/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
@@ -15,9 +16,14 @@ public class SecurityConfig {
             throws Exception {
         return httpSecurity
                 .authorizeHttpRequests(oauth -> oauth.anyRequest().authenticated())
-                .oauth2Login(oauth2login -> oauth2login.defaultSuccessUrl("/user-info",true))
-                .formLogin(formLogin-> formLogin.defaultSuccessUrl("/secured",true))
+                .oauth2Login(oauth2login -> oauth2login.defaultSuccessUrl("/contacts",true))
+               // .formLogin(formLogin-> formLogin.defaultSuccessUrl("/secured",true))
                 .logout(logout-> logout.logoutSuccessUrl("/"))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
-    }}
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
