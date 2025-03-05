dff630063d97b195109451ecf179fdf13bfaddc8
BanDev <ivanjayadoptante@gmail.com>
2025-03-05 23:57:40 +0800

    Added The Google Contacts Integration

---
+++ oauth2login/src/main/java/com/handayan/oauth2login/Controller/UserController.java
@@ -0,0 +1,24 @@
+package com.handayan.oauth2login.Controller;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RestController
+public class UserController{
+    @GetMapping
+    public String index(){
+        return "Hello I am Amiel Kenth Handayan";
+
+    }
+    @GetMapping("user-info")
+    public Map<String, Object> getUser(@AuthenticationPrincipal OAuth2User principal){
+        return principal.getAttributes();
+
+    }
+}
