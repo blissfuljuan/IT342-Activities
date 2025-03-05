5782dabc7620d701b2d538f6c182656c6e9dec9f
SaddenedMigsu <miggyjaca@gmail.com>
2025-03-05 17:40:07 +0800

    Activity: Google Contacts/People API Integration

--- g4-jaca-luismiguel/oauth2login/src/main/java/com/jaca/oauth2login/config/SecurityConfig.java
+++ g4-jaca-luismiguel/oauth2login/src/main/java/com/jaca/oauth2login/config/SecurityConfig.java
@@ -1,26 +1,35 @@
 package com.jaca.oauth2login.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:8080/user-info", true))
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/user-info", true)
+                )
                 .logout(logout -> logout.logoutSuccessUrl("/"))
-                .formLogin(form -> form.defaultSuccessUrl("/secured"))
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
+                .formLogin(form -> form.defaultSuccessUrl("/user-info", true));
+
+        return http.build();
     }
+
+
+
 }
