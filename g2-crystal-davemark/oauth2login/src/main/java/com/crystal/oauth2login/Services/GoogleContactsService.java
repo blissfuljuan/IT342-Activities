d3b0aaeef04cf9ea130121f4fd73c7938ceea24f
LAB13_USER ON WS09 <L13X13W09@citulab.net>
2025-03-05 14:03:29 +0800

    Fixed the css and html template

--- g2-crystal-davemark/oauth2login/src/main/java/com/crystal/oauth2login/Services/GoogleContactsService.java
+++ g2-crystal-davemark/oauth2login/src/main/java/com/crystal/oauth2login/Services/GoogleContactsService.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +23,7 @@ public class GoogleContactsService {
     }
 
     private String getAccessToken() {
+
         Authentication authentication = org.springframework.security.core.context.SecurityContextHolder.getContext().getAuthentication();
         if (authentication instanceof OAuth2AuthenticationToken) {
             OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
