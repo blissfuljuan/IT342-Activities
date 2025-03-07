8ded32a7155c14dc5faf20878acbe49954fd7bef
xxmu53xx <73223072+xxmu53xx@users.noreply.github.com>
2025-03-07 14:18:48 +0800

    fix googleContacts CRUD

--- g7-largo-johnwayne/GoogleContacts/src/main/java/com/Largo/GoogleContacts/model/Contacts.java
+++ g7-largo-johnwayne/GoogleContacts/src/main/java/com/Largo/GoogleContacts/model/Contacts.java
@@ -1,49 +1,46 @@
 package com.Largo.GoogleContacts.model;
 
 public class Contacts {
-    private String id;
+    private String resourceName;
     private String name;
     private String email;
     private String phoneNumber;
-
-    public Contacts() {}
-
-    public Contacts(String id, String name, String email, String phoneNumber) {
-        this.id = id;
+    
+    public Contacts() {
+    }
+    public Contacts(String name, String email) {
         this.name = name;
         this.email = email;
-        this.phoneNumber = phoneNumber;
     }
 
-    // Getters and setters
-    public String getId() {
-        return id;
+    public String getResourceName() {
+        return resourceName;
     }
-
-    public void setId(String id) {
-        this.id = id;
+    
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public String getEmail() {
         return email;
     }
-
+    
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
+    
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
