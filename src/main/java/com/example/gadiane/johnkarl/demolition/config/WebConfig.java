35496d48cbf4a02b7720e87f0b8c8fa9a10b20b8
SZRIIN GADIANE <jkpgadiane@gmail.com>
2025-03-08 02:54:02 +0800

    hahays

--- src/main/java/com/example/gadiane/johnkarl/demolition/config/WebConfig.java
+++
@@ -1,4 +0,0 @@
-package com.example.gadiane.johnkarl.demolition.config;
-
-public class WebConfig {
-}
---
+++ src/main/java/com/gadiane/johnkarl/demolition/DemolitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadiane.johnkarl.demolition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolitionApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemolitionApplicationTests.class, args);
+	}
+
+}
