ef2eb720a7c1f2ba36a95ce4568cbd7660af8e68
John Baclayon <johnmichael.baclayon@cit.edu>
2025-03-07 14:48:18 +0800

    Activity: Google Contacts/People API Integration Midterm Practical Exam by John Michael Baclayon

--- g1-baclayon-johnmichael/oauth2login/src/main/java/com/baclayon/oauth2login/Oauth2loginApplication.java
+++ g1-baclayon-johnmichael/oauth2login/src/main/java/com/baclayon/oauth2login/Oauth2loginApplication.java
@@ -2,6 +2,7 @@ package com.baclayon.oauth2login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Oauth2loginApplication {
@@ -9,7 +10,4 @@ public class Oauth2loginApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2loginApplication.class, args);
 	}
-
-
 }
- 
