7a0bf02ce3e5729959ff16cffe3ed07a8d2fd8a6
Avien <ejaysolasco09@gmail.com>
2025-03-07 10:51:33 +0800

    GOOGLE CONTACTS

---
+++ g1-solasco-ephraimjay/googlecontact/src/test/java/com/solasco/googlecontact/GoogleContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solasco.googlecontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
