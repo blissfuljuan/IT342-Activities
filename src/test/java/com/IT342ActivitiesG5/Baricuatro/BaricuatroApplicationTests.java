84e61f8672a9bdc23b78fc6fb5a4b9a2b3f54781
Karu <169058702+KArruuuu@users.noreply.github.com>
2025-03-10 03:39:31 +0800

    Midterm_G_Auth

---
+++ src/test/java/com/IT342ActivitiesG5/Baricuatro/BaricuatroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IT342ActivitiesG5.Baricuatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaricuatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
