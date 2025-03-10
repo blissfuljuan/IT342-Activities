43d6834956dfd5011447375d3e6f7d4df9304cfd
LAB22_USER ON WS22 <L22X07W22@citulab.net>
2025-03-10 08:48:01 +0800

    Updated UserController to access User Info

--- DelMar_Login/DelMar_Login/src/main/java/com/example/demo/config/SecurityConfig.java
+++ DelMar_Login/DelMar_Login/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,11 +1,10 @@
-package config;
+package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
