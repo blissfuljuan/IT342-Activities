10ccbeb508ad15c8306d231f8c2bcd08720e51af
Frederick Revilleza <frederick.revilleza@citulab.net>
2025-03-10 10:27:47 +0800

    add database connection

--- g7-revilleza-frederick/oauth2login/src/main/java/com/revilleza/oauth2login/controller/UserController.java
+++ g7-revilleza-frederick/oauth2login/src/main/java/com/revilleza/oauth2login/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.revilleza.oauth2login.controller;
 
+import com.revilleza.oauth2login.model.User;
+import com.revilleza.oauth2login.repository.UserRepository;
 import com.revilleza.oauth2login.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,8 @@ import java.util.Map;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public String index(){
@@ -21,7 +26,11 @@ public class UserController {
     @GetMapping("/user-info")
     public String getUser(@AuthenticationPrincipal OAuth2User oAuth2User, Model model){
         if( oAuth2User != null) {
-            model.addAttribute("user", Utils.OAuth2UserToUser(oAuth2User));
+            User user = Utils.OAuth2UserToUser(oAuth2User);
+            User currentUser = userRepository.findByEmail(user.getEmail());
+            if(currentUser==null)
+                userRepository.save(user);
+            model.addAttribute("user", user);
         } else {
             model.addAttribute("user", Collections.emptyMap());
         }
--- g7-revilleza-frederick/oauth2login/src/main/java/com/revilleza/oauth2login/model/User.java
+++ g7-revilleza-frederick/oauth2login/src/main/java/com/revilleza/oauth2login/model/User.java
@@ -1,6 +1,15 @@
 package com.revilleza.oauth2login.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     private String firstName;
     private String lastName;
     private String displayName;
